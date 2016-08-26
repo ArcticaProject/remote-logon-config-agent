1a13704532d6118881fa74f30ff76e44d569b507
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2016-08-26 15:20:38 +0200

    rename thin(-|)client-config-agent script to remote-logon-config-agent (as it is a helper tool for the remote-logon-service utilized by Arctica Greeter

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 import re
 
 setup(
-    name='thinclient-config-agent',
+    name='remote-logon-config-agent',
     version=re.search(r'\((.*)\)', open('debian/changelog').readline()).group(1),
     license='GPL',
     description='Retrieve the list of remote desktop servers for a user.',
